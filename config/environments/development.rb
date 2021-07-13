1615f70496dd8c14a548a94de53776cb48ab3b30
hiroear <clan_internal@outlook.jp>
2021-07-14 04:28:12 +0900

    環境変数・メールを修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,8 +46,8 @@ Rails.application.configure do
     :address => "smtp.gmail.com",
     :port => 587,
     :domain => 'smtp.gmail.com',
-    :user_name => ENV["clan.fukoffice@gmail.com"],
-    :password => ENV["nyhfaqsyklsdbumy"],
+    :user_name => ENV["GOOGLE_MAIL_ADDRESS"],
+    :password => ENV["GOOGLE_MAILER_PASSWORD"],
     :authentication => 'login'
   }
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,7 +25,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   # メールを送信するアドレス
-  config.mailer_sender = ENV["clan.fukoffice@gmail.com"]
+  config.mailer_sender = ENV["GOOGLE_MAIL_ADDRESS"]
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
